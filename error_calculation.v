30df02a939ffff3a1ffd07e6e0fd6f1bda0429c1
Manthan-cpu <ee240002036@iiti.ac.in>
2026-04-25 21:58:56 +0530

    Update error_calculation.v

--- error_calculation.v
+++ error_calculation.v
@@ -9,7 +9,7 @@ module voltage_scaling #(parameter V_ref = 3072)
     input [11:0] adc_in,
     output reg signed [12:0] error
 );
-   //  reg[12:0] error1=0;
+ 
 always @(posedge clk or posedge reset)
 begin
     
@@ -19,7 +19,7 @@ begin
     else
         error <= V_ref - adc_in;
         
-   // error <= error1[12:1];
+  
 end
 
 endmodule
